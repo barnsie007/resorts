0567c30c114f576bd77013ebf52430aed20ae88d
TransformationAdvisor <transformation.advisor@ie.ibm.com>
2024-04-24 11:06:08 -0700

    resorts

---
+++ src/test/java/com/acme/modres/mbean/DMBeanUtilsTest.java
@@ -0,0 +1,12 @@
+package com.acme.modres.mbean;
+
+import javax.management.MBeanOperationInfo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DMBeanUtilsTest {
+
+	
+}
